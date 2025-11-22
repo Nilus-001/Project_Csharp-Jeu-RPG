4d2422696a4f9aa7b92da0d5dcd018f5e16952e2
NilsLW <nils.levequew@gmail.com>
2025-11-22 18:44:58 +0100

    addition of Info_Enumerated.cs : enumeration of all the Rarity

---
+++ Project_RPG_Game/src/Info_Enumerated.cs
@@ -0,0 +1,29 @@
+﻿namespace Project_RPG_Game;
+
+
+
+public enum Rarity {
+    Classic,
+    Common,
+    Rare,
+    Epic,
+    Legendary,
+    Mythic
+}
+
+public enum BonusMissions {
+    
+}
+public enum MalusMissions {
+    
+}
+public enum LongTimeMissions {
+    
+}
+
+public enum BonusEvents {
+    
+}
+public enum MalusEvents {
+    
+}

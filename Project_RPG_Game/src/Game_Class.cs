f3f4c2409a90863c3c341edb2eed86ee4a2455c4
NilsLW <nils.levequew@gmail.com>
2025-11-22 18:43:24 +0100

    addintion of various class : - Equipment - GameClass - Race with no function or attribut

---
+++ Project_RPG_Game/src/Game_Class.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_RPG_Game;
+
+public class GameClass {
+    private static int id_increment = 0;
+    public int id;
+
+    public GameClass() {
+        id_increment++;
+        id = id_increment;
+    }
+}
---
+++ Project_RPG_Game/src/Race.cs
@@ -0,0 +1,6 @@
+﻿namespace Project_RPG_Game;
+
+public class Race {
+    private static int id_increment = 0;
+    public int id;
+}

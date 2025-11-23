510c4ab2477f9c25fec90bbb0f4207c8218019cf
NilsLW <nils.levequew@gmail.com>
2025-11-23 15:43:15 +0100

    Adding class Mission with their attribute

---
+++ Project_RPG_Game/src/missions/Mission.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Project_RPG_Game.missions.Lose;
+using Project_RPG_Game.missions.Success;
+
+namespace Project_RPG_Game.missions;
+
+public abstract class Mission {
+    public string Name;
+    public string Description;
+    public Difficulty Difficulty;
+    public int NbHero;
+    public List<MissionType> Types;   //! setup required in info_enumerated
+    public List<Hero> ActiveHeroes;
+    public SuccessResult SuccessResult; //! setup class SuccessResult
+    public LoseResult LoseResult;   //! setup class LoseResult
+
+}

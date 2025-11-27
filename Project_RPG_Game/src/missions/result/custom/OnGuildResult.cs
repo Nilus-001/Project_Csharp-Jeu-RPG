90a691c025d3df7c84df4a666d874adecb079dc1
NilsLW <nils.levequew@gmail.com>
2025-11-27 21:38:57 +0100

    Adding OnCharacterResult class : Result's child - with all fonctionnality Adding OnGuildResult class : empty

---
+++ Project_RPG_Game/src/missions/result/custom/OnGuildResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Project_RPG_Game.missions.result;
+
+public class OnGuildResult:Result.Result {
+    
+    
+    
+    public override Dictionary<Hero, Dictionary<string, object>> ExecuteResult(Mission mission) {
+        throw new System.NotImplementedException();
+    }
+}

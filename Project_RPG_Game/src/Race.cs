ef56f1f3271acae985ae8493bcbbbef497a584a5
NilsLW <nils.levequew@gmail.com>
2025-11-27 21:42:46 +0100

    Implementing race with all fonctionnality

--- Project_RPG_Game/src/Race.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Project_RPG_Game;
-
-public class Race {
-    private static int id_increment = 0;
-    public int id;
-}
---
+++ Project_RPG_Game/src/races/Race.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Project_RPG_Game.missions;
+
+namespace Project_RPG_Game;
+
+public class Race {
+    private static int _idincrement = 0;
+    public int Id;
+    public Dictionary<TerrainType,int> TerrainModifier;
+
+    public Race(Dictionary<TerrainType, int> typesModifier) {
+        TerrainModifier = typesModifier;
+        
+        _idincrement++;
+        Id = _idincrement;
+    }
+
+    public int GetPercetageModifier(Mission mission) {
+        int percentageBonus = 0;
+        foreach (var terrain in TerrainModifier) {
+            if (terrain.Key == mission.TerrainType) {
+                percentageBonus += terrain.Value;
+            }
+        }
+        return percentageBonus;
+    }
+    
+    
+}

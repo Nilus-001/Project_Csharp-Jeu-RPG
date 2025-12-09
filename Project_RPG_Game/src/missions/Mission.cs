213d85e832517d4be41d7f1a36ee1e166ab88e1d
NilsLW <nils.levequew@gmail.com>
2025-12-09 21:55:12 +0100

    Adding mission result system

--- Project_RPG_Game/src/missions/Mission.cs
+++ Project_RPG_Game/src/missions/Mission.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Project_RPG_Game.characters;
 using Project_RPG_Game.missions.Result;
+using Project_RPG_Game.status.custom;
 
 
 namespace Project_RPG_Game.missions;
@@ -12,16 +15,28 @@ public class Mission {
     public string Name;
     public string Description;
     public Difficulty Difficulty;
+    public int ExecutionTimer;
     public int NbHero;
     public MissionType Type;
     public TerrainType TerrainType;
-    public List<Hero> ActiveHeroes;
-    public Result.Result SuccessResult; //! setup class Result
+    public List<Hero> ActiveHeroes = new List<Hero>();
+    public Result.Result SuccessResult;
     public string SuccessDescritpion;
     public Result.Result LoseResult;   
     public string LoseDescritpion;
 
-    public Mission(string name, string description, Difficulty difficulty, int nbHero, MissionType type, TerrainType terrainType, Result.Result successResult, string successDescritpion, Result.Result loseResult, string loseDescritpion) {
+    public Mission(string name,
+        string description,
+        Difficulty difficulty,
+        int nbHero,
+        MissionType type,
+        TerrainType terrainType, 
+        Result.Result successResult,
+        string successDescritpion,
+        Result.Result loseResult,
+        string loseDescritpion,
+        int executionTimer = 1) {
+        
         Name = name;
         Description = description;
         Difficulty = difficulty;
@@ -32,36 +47,46 @@ public class Mission {
         SuccessDescritpion = successDescritpion;
         LoseResult = loseResult;
         LoseDescritpion = loseDescritpion;
-        
+        ExecutionTimer = executionTimer;
+
         _idIncrement++;
         Id = _idIncrement;
     }
 
-    public bool SetActiveHero(Hero hero) {
-        if (ActiveHeroes.Count < NbHero && !ActiveHeroes.Contains(hero)) {
+    public void SetActiveHero(Hero hero) {
+        if (ActiveHeroes.Count < NbHero && hero.StatusIsActive(hero ,typeof(Fatigue)) == null) {
             ActiveHeroes.Add(hero);
-            return true;
         }
-        return false;   
+        
     }
     
     
     
 
-    public Result.Result ReturnResult() {
-        int heroPercentageModifier = 0;
+    public ArrayList ExecuteResult() {
+        ArrayList resume = new ArrayList();
+        
+        int heroBonusLuck = 0;
+        string bonusEventClass = "";
         foreach (var hero in ActiveHeroes) {
-            heroPercentageModifier += hero.GetPercentageModifier(this);
+            heroBonusLuck += hero.GetBonusLuck(this);
+            bonusEventClass += "///"+hero.GameClass.BonusResult(this);
         }
         
-        bool success = Global.DiceRoll((int)Difficulty + heroPercentageModifier);
+        
+        
+        bool success = Global.DiceRoll((int)Difficulty + heroBonusLuck);
         if (success) {
-            //! to do print Success Description
-            return SuccessResult;
+            resume.Add(SuccessDescritpion);
+            resume.Add(SuccessResult.ExecuteResult(this));
+            
+        }else{
+             resume.Add(LoseDescritpion);
+             resume.Add(LoseResult.ExecuteResult(this));
         }
         
-        //! to do print Lose Description
-        return LoseResult;
+        resume.Add(bonusEventClass);
+        return resume;
         
     }
 

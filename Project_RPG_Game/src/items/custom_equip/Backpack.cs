e967323b0b83a662595e0c0d36172de9e3009b45
NilsLW <nils.levequew@gmail.com>
2026-01-06 11:37:10 +0100

    Fix backpack

--- Project_RPG_Game/src/items/custom_equip/Backpack.cs
+++ Project_RPG_Game/src/items/custom_equip/Backpack.cs
@@ -8,7 +8,7 @@ public class Backpack : Equipment , IBonusItem , IResultBonusOnGuild {
     public int CapacityPercentageBonus;
     
     public Backpack(string name,  Rarity rarity) : base(name, $"Grant +{(int)rarity}% of Bonus FoodStock for the Guild During Missions", rarity,"Project_RPG_Game/assets/item/ItemBackpack.png") {
-        CapacityPercentageBonus = (int)rarity;;
+        CapacityPercentageBonus = (int)rarity/100;
     }
     
 
